b6d7da9f6231dd46036178ac1e4e849be1fe42be
joshinakul <nakulj3010@outlook.com>
2019-03-04 16:01:24 +0530

    done

--- recognize.py
+++ recognize.py
@@ -1,49 +1,45 @@
 import cv2
-import sqlite3
+import sqlite3 as s
 cap = cv2.VideoCapture(0)
 
 # Create the haar cascade
 faceCascade = cv2.CascadeClassifier("myfacehaarcascade.xml")
-rec=cv2.face.LBPHFaceRecognizer_create()
-rec.read("recognizer/trainer.yml")
+model = cv2.face.LBPHFaceRecognizer_create()
+model.read('recognizer/trainer.yml')
 font=cv2.FONT_HERSHEY_DUPLEX
 path="dataSet"
 
-def getProfile(id):
-	con=sqlite3.connect("data_persons.db")
-	cmd="SELECT * FROM data WHERE ID="+str(id)
-	cursor=con.execute(cmd)
-	profile=None
-	for row in cursor:
-		profile=row
-	con.close()
-	return profile
+def getProfile(idx):
+    
+    con = s.connect('data_persons.db')
+    cursor = con.cursor()
+    c = cursor.execute('SELECT * FROM data WHERE ID='+str(idx))
+    for row in c:
+        profile_tuple = row
+        return profile_tuple
+
 while(True):
 	# Capture frame-by-frame
     ret, frame = cap.read()
     gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
 	# Detect faces in the image
-    faces = faceCascade.detectMultiScale(frame,1.05,4)
+    faces = faceCascade.detectMultiScale(frame,1.5,5)
 
 
-    print("Found {0} faces!".format(len(faces)))
-
-	# Draw a rectangle around the faces
-    flag=0
-    i=""
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x ,y), (x+w, y+h), (223, 200, 31), 2)
-        idx,config=rec.predict(gray[y:y+h,x:x+w])
-        if config<85:
-            profile=getProfile(idx)
+        idx,config=model.predict(gray[y:y+h,x:x+w])
+        profile=getProfile(idx)
+        if config > 65:
+            print(profile)
         
         if profile!=None:
-            cv2.rectangle(frame, (x ,y), (x+w, y+h), (223, 200, 31), 2)
-            cv2.putText(frame,str(profile[1]),(x,y+h),font,1,(0,255,0))
+            cv2.rectangle(frame, (x ,y), (x+w, y+h), (123,100, 61), 2)
+            cv2.putText(frame,profile[1],(x,y+h),font,1,(0,255,0))
 			
 	# Display the resulting frame
     cv2.imshow('Detection Required', frame)
-    if flag==1 or cv2.waitKey(10) == 27:
+    if cv2.waitKey(1) == 27:
 		# When everything done, release the capture
         cap.release()
         cv2.destroyAllWindows()
